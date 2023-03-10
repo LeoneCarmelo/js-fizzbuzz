64439197b204b0fbb975633d0bbc553272ac744c
LeoneCarmelo <carmeloleone@outlook.it>
2023-03-10 15:02:18 +0100

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+/* 
+Scrivi un programma che stampi in console i numeri da 1 a 100 ma che:
+1 per i multipli di 3 stampi “Fizz” al posto del numero
+2 per i multipli di 5 stampi “Buzz”
+3 per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”. */
